6538ee389a5bc3460f63a1d21a2879200c01fb82
Iezid <iezidjesus@gmail.com>
2023-09-22 21:53:49 -0500

    SAVEDRITATRAGASABLE

---
+++ src/main/java/com/project/consorcio/examen/entity/saavedracabro.java
@@ -0,0 +1,15 @@
+package com.project.consorcio.examen.entity;
+
+public class saavedracabro {
+
+	private String saavedracabro;
+
+	public String getSaavedracabro() {
+		return saavedracabro;
+	}
+
+	public void setSaavedracabro(String saavedracabro) {
+		this.saavedracabro = saavedracabro;
+	}
+	
+}
