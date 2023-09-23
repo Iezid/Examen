b02328b74ce82b989563fcc8d1ab19e64d9fe451
Iezid <iezidjesus@gmail.com>
2023-09-22 22:04:58 -0500

    Triste

---
+++ src/main/java/com/project/consorcio/examen/entity/IezidyNahomi.java
@@ -0,0 +1,19 @@
+package com.project.consorcio.examen.entity;
+
+public class IezidyNahomi {
+private String iezid;
+private String jesus;
+public String getIezid() {
+	return iezid;
+}
+public void setIezid(String iezid) {
+	this.iezid = iezid;
+}
+public String getJesus() {
+	return jesus;
+}
+public void setJesus(String jesus) {
+	this.jesus = jesus;
+}
+
+}
